544a112e9b54cc312c99eb10ed47b440cb5a518f
DennisHab <74969035+DennisHab@users.noreply.github.com>
2021-07-22 11:44:37 +0200

    React language context d (#1)
    * made language switch
    
    * Added language switch

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 import headerImage from '../../assets/wall-with-plants.jpg';
 import { ReactComponent as FlagNL } from '../../assets/netherlands.svg';
+import { ReactComponent as FlagES } from '../../assets/spain.svg'
 import content from '../../data/content.json';
 import './Header.css';
+import {LanguageContext} from "../../context/LanguageContext";
+
 
 function Header() {
+  const {activeLanguage, changeLanguage} = useContext(LanguageContext)
+
   return (
     <>
       <header>
@@ -13,23 +18,30 @@ function Header() {
           <ul>
             <li>
               <NavLink exact to="/about-us">
-                {content.nl.header.menuItems.aboutUs}
+                {content[activeLanguage].header.menuItems.aboutUs}
               </NavLink>
             </li>
             <li>
               <NavLink exact to="/all-plants">
-                {content.nl.header.menuItems.allPlants}
+                {content[activeLanguage].header.menuItems.allPlants}
               </NavLink>
               </li>
             <li className="language-switch">
-              <p>{content.nl.header.changeTo}</p>
-              <FlagNL />
+              <p>{activeLanguage === "nl" ? "Taal" : "Idioma"}</p>
+              <div className="language-nl" onClick= {()=>changeLanguage('nl')} style={{opacity: activeLanguage === 'es' && '20%'}}>
+                <FlagNL
+                />
+              </div>
+              <div className="language-es" onClick= {()=>changeLanguage('es')} style={{opacity: activeLanguage === 'nl' && '20%'}}>
+                <FlagES
+                />
+              </div>
             </li>
           </ul>
         </nav>
         <div className="image-container">
           <img src={headerImage} alt="Header image plants" className="header-image" />
-          <h1>{content.nl.header.title}</h1>
+          <h1>{content[activeLanguage].header.title}</h1>
         </div>
 
       </header>
